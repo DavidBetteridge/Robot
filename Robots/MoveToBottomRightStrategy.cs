cc654270842f8a62c3d023af0c83d217974baaba
David Betteridge <david.betteridge@proactis.com>
2020-11-01 21:34:21 +0000

    Optimise by missing out rows already checked

--- Robots/MoveToBottomRightStrategy.cs
+++ Robots/MoveToBottomRightStrategy.cs
@@ -3,12 +3,18 @@
     internal class MoveToBottomRightStrategy : IStrategy
     {
         private Direction xDirection = Direction.Right;
+        private int downLeftToMove = 0;
         public Direction? SuggestDirection(Robot robot)
         {
             var direction = robot.DirectionOfTreasure();
             if (direction is object)
             {
                 return direction.Value;
+            }
+            else if (downLeftToMove > 0)
+            {
+                downLeftToMove--;
+                return Direction.Down;
             }
             else if (robot.Look(xDirection) == Content.Empty)
             {
@@ -17,6 +23,7 @@
             else if (robot.Look(Direction.Down) == Content.Empty)
             {
                 ToggleLeftRight();
+                downLeftToMove = 2;
                 return Direction.Down;
             }
             else
