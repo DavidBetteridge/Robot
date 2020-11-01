7538cc33e7e1f3922745c790b797aabf4d98576b
David Betteridge <david.betteridge@proactis.com>
2020-11-01 23:14:19 +0000

    Change complicated logic into more of a state machine

--- Robots/MoveToBottomRightStrategy.cs
+++ Robots/MoveToBottomRightStrategy.cs
@@ -2,9 +2,19 @@
 {
     internal class MoveToBottomRightStrategy : IStrategy
     {
-        private Direction xDirection = Direction.Right;
-        private int downLeftToMove = 0;
+        private enum State
+        {
+            FindingRightEdge,
+            HeadingRight,
+            HeadingDownThenLeft,
+            HeadingDownThenRight,
+            HeadingLeft
+        }
+
+        private State _currentState = State.FindingRightEdge;
+
         private int xOffset = 0;
+        private int yOffset = 0;
         private int maxXOffset = int.MaxValue;
         public Direction? SuggestDirection(Robot robot)
         {
@@ -13,48 +23,146 @@
             {
                 return direction.Value;
             }
-            else if (downLeftToMove > 0 && robot.Look(Direction.Down) == Content.Empty)
-            {
-                downLeftToMove--;
-                return Direction.Down;
-            }
-
-            else if (xDirection == Direction.Left && xOffset > 0)
-            {
-                xOffset--;
-                return Direction.Left;
-            }
-
-            else if (xDirection == Direction.Right && xOffset < maxXOffset && robot.Look(Direction.Right) == Content.Empty)
-            {
-                xOffset++;
-                return Direction.Right;
-            }
-
-            else if (xDirection == Direction.Right && robot.Look(Direction.Right) != Content.Empty)
-            {
-                xOffset--;
-                maxXOffset = xOffset;
-                ToggleLeftRight();
-                downLeftToMove = 3;
-                return Direction.Left;
-            }
-
-            else if (robot.Look(Direction.Down) == Content.Empty)
-            {
-                ToggleLeftRight();
-                downLeftToMove = 2;
-                return Direction.Down;
-            }
             else
             {
-                return null;
+                switch (_currentState)
+                {
+                    case State.FindingRightEdge:
+                        if (AtRightEdge(robot))
+                        {
+                            RecordLocationOfRightEdge();
+                            ChangeToHeadingDownThenLeft(numberToGoDown: 3);
+                            return MoveLeft();
+                        }
+                        else
+                        {
+                            return MoveRight();
+                        }
+
+                    case State.HeadingRight:
+                        if (CanHeadRight())
+                        {
+                            return MoveRight();
+                        }
+                        else
+                        {
+                            ChangeToHeadingDownThenLeft(numberToGoDown: 2);
+                            return MoveDownIfPossible(robot);
+                        }
+
+                    case State.HeadingDownThenLeft:
+                        if (ShouldContinueHeadingDown(robot))
+                        {
+                            return MoveDown();
+                        }
+                        else
+                        {
+                            ChangeToHeadingLeft();
+                            return MoveLeft();
+                        }
+
+                    case State.HeadingLeft:
+                        if (CanHeadLeft())
+                        {
+                            return MoveLeft();
+                        }
+                        else
+                        {
+                            ChangeToHeadingDownThenRight(2);
+                            return MoveDownIfPossible(robot);
+                        }
+
+                    case State.HeadingDownThenRight:
+                        if (ShouldContinueHeadingDown(robot))
+                        {
+                            return MoveDown();
+                        }
+                        else
+                        {
+                            ChangeToHeadingRight();
+                            return MoveRight();
+                        }
+
+                    default:
+                        break;
+                }
             }
+
+            return null;
         }
 
-        private void ToggleLeftRight()
+        private void ChangeToHeadingRight()
         {
-            xDirection = xDirection == Direction.Left ? Direction.Right : Direction.Left;
+            _currentState = State.HeadingRight;
+        }
+
+        private void ChangeToHeadingLeft()
+        {
+            _currentState = State.HeadingLeft;
+        }
+
+        private void RecordLocationOfRightEdge()
+        {
+            maxXOffset = xOffset - 1;
+        }
+
+        private void ChangeToHeadingDownThenLeft(int numberToGoDown)
+        {
+            yOffset = numberToGoDown;
+            _currentState = State.HeadingDownThenLeft;
+        }
+
+        private void ChangeToHeadingDownThenRight(int numberToGoDown)
+        {
+            yOffset = numberToGoDown;
+            _currentState = State.HeadingDownThenRight;
+        }
+
+
+        private bool CanHeadRight()
+        {
+            return xOffset < maxXOffset;
+        }
+
+        private bool CanHeadLeft()
+        {
+            return xOffset > 0;
+        }
+
+        private bool ShouldContinueHeadingDown(Robot robot)
+        {
+            return yOffset > 0 && robot.Look(Direction.Down) == Content.Empty;
+        }
+
+        private static bool AtRightEdge(Robot robot)
+        {
+            return robot.Look(Direction.Right) == Content.Wall;
+        }
+
+        private static Direction? MoveDownIfPossible(Robot robot)
+        {
+            if (robot.Look(Direction.Down) != Content.Empty)
+                return null;
+            else
+                return Direction.Down;
+        }
+
+        private Direction MoveRight()
+        {
+            xOffset++;
+            return Direction.Right;
+        }
+
+        private Direction MoveLeft()
+        {
+            xOffset--;
+            return Direction.Left;
+        }
+
+        private Direction MoveDown()
+        {
+            yOffset--;
+            return Direction.Down;
         }
     }
 
